c420a9fffb52dba4b096223981491d6ed698ad1a
harryli0088 <harryli0088@gmail.com>
2020-09-10 22:29:52 -0400

    now next value in sender

--- src/Components/Sender/Sender.tsx
+++ src/Components/Sender/Sender.tsx
@@ -10,7 +10,9 @@ interface Props {
   value: number,
 }
 
-interface State {}
+interface State {
+  nextValue: number,
+}
 
 export default class Sender extends React.Component<Props,State> {
   valueGenerator: ValueGeneratorType
@@ -20,25 +22,43 @@ export default class Sender extends React.Component<Props,State> {
 
     this.valueGenerator = getValueGenerator(props.generator, props.seed)
 
-    this.state = {}
+    this.state = {
+      nextValue: this.valueGenerator(),
+    }
   }
 
   componentDidUpdate(prevProps:Props) {
     if(prevProps.generator !== this.props.generator) {
       this.valueGenerator = getValueGenerator(this.props.generator, this.props.seed)
+
+      this.setState({
+        nextValue: this.valueGenerator(), //calculate the next value
+      })
     }
+  }
+
+  onClick = () => {
+    this.props.clickSenderCallback(this.state.nextValue) //send the next value to the parent
+
+    this.setState({
+      nextValue: this.valueGenerator(), //calculate the next value
+    })
   }
 
   render() {
     const {
-      clickSenderCallback,
       value,
     } = this.props
+
+    const {
+      nextValue,
+    } = this.state
 
     return (
       <div className="sender">
         <div className="value">{value>0 ? value : "-"}</div>
-        <button onClick={e => clickSenderCallback(this.valueGenerator())}>Lock/Unlock</button>
+        <div className="nextValue"><b>Next Value: </b>{nextValue}</div>
+        <button onClick={e => this.onClick()}>Lock/Unlock</button>
       </div>
     )
   }
